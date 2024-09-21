75e7cee8415377a47b19fd700331a424d2a80c62
faysal mohammed <fayomuhe5@gmail.com>
2024-09-21 09:55:42 +0300

    input field fixed

---
+++ storage/framework/views/ee6d6f080b29cab710eaed3f82b5efb7.php
@@ -0,0 +1,5 @@
+<span <?php echo e($attributes); ?> data-tooltip="<?php echo e(__('common.edit')); ?>" data-variation="mini">
+    <a href="<?php echo e($route); ?>" style="color: inherit">
+        <i class="pen alternate link icon hover:text-blue-500"></i>
+    </a>
+</span><?php /**PATH /var/www/html/resources/views/components/edit-button.blade.php ENDPATH**/ ?>
